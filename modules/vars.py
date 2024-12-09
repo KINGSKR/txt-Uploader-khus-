8a7474033a4bfb03968129c9e2b51fbb8c01205a
rajbhai <167851226+KINGSKR@users.noreply.github.com>
2024-12-09 15:09:25 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "23537514"
 api_hash = "c5ec016af7d2cb637278b715f4f5a389"
-bot_token = "7651473687:AAErBUj1_MW3lobYbdKjcM9BGkTyBRfriek"
+bot_token = "6838940253:AAF6GVR63tZ9FfZZjnbWVGaf8iDQ5YEjLT8"
